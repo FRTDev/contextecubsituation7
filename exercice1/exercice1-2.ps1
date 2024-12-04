cb63cb89ef846880cbd9ca60d8df9591075757d5
Noah Froment <noah.froment@btssio.lan>
2024-12-04 09:10:43 +0100

    exercice1-2.ps1

---
+++ exercice1/exercice1-2.ps1
@@ -0,0 +1,9 @@
+==========================================================================
+#Description : Lister les numéros d'id et le noms des processus "svchost"
+
+#Auteur : Noah Froment
+#Date : 04/12/2024
+
+#Version : 1
+==========================================================================
+
