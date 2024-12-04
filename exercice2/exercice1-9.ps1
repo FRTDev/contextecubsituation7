156c3a37560a7c8c3b395d397e3bc28bd07df228
Noah Froment <noah.froment@btssio.lan>
2024-12-04 11:32:14 +0100

    .

---
+++ exercice2/exercice1-9.ps1
@@ -0,0 +1,22 @@
+<#
+==========================================================================
+Description : Tous les services qui contient "system"
+
+Auteur : Noah Froment
+Date : 04/12/2024
+
+Version : 1
+==========================================================================
+#>
+
+$ServicesCSV = Import-Csv -Path "C:\Users\noah.froment\Desktop\AdminSys_Powershell\contextecubsituation7\exercice2\resultats\services.csv"
+
+$ServicesCSV | Format-Table
+
+
+
+
+
+
+
+
