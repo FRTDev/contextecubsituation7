825bc939a20cc8e915ab45fe05c805ce539a1b1a
Noah Froment <noah.froment@btssio.lan>
2024-12-04 11:43:31 +0100

    .

--- exercice2/exercice1-9.ps1
+++ exercice2/exercice1-9.ps1
@@ -1,6 +1,6 @@
 <#
 ==========================================================================
-Description : Tous les services qui contient "system"
+Description : Tous les services qui contient "system" coloré en vert 
 
 Auteur : Noah Froment
 Date : 04/12/2024
@@ -9,9 +9,20 @@ Version : 1
 ==========================================================================
 #>
 
+# Importer le fichier CSV
 $ServicesCSV = Import-Csv -Path "C:\Users\noah.froment\Desktop\AdminSys_Powershell\contextecubsituation7\exercice2\resultats\services.csv"
 
-$ServicesCSV | Format-Table
+# Parcourir chaque ligne du CSV
+foreach ($Service in $ServicesCSV) {
+    # Vérifier si le nom du service contient "system"
+    if ($Service.Name -like "*system*") {
+        # Afficher le nom et le statut du service en vert
+        Write-Host ($Service.Name, $Service.Status) -ForegroundColor Green
+    }
+    else {
+        Write-Host $Service.Name
+    }
+}
 
 
 
