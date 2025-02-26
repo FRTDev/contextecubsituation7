1554d47abfca630a306f21abb2b96acafaac54d2
FRT Dev <52767575+FRTDev@users.noreply.github.com>
2025-02-26 09:06:15 +0100

    .

--- exercice1/exercice1-7.ps1
+++ exercice1/exercice1-7.ps1
@@ -2,7 +2,7 @@
 ==========================================================================
 Description : Lister les services (nom, description et status) et l'enregistrer dans un fichier .csv
 
-Auteur : Noah Froment
+Auteur : FRTDev
 Date : 04/12/2024
 
 Version : 1
@@ -11,8 +11,8 @@ Version : 1
 
 $resultats = Get-Service | Select-Object Status, Name, DisplayName
 
-# Enregistrer les résultats dans un fichier CSV
+# Enregistrer les rÃ©sultats dans un fichier CSV
 $resultats | Export-CSV -Path "C:\Users\noah.froment\Desktop\AdminSys_Powershell\contextecubsituation7\exercice1\resultats\services.csv" -NoTypeInformation
 
-# Afficher le résulat dans la console
+# Afficher le rÃ©sulat dans la console
 Write-Output $resultats
