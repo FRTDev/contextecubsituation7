323c130e73be05f7d8190a63b138cebe14b7caa0
FRT Dev <52767575+FRTDev@users.noreply.github.com>
2025-02-26 09:07:29 +0100

    .

--- exercice2/exercice1-10.ps1
+++ exercice2/exercice1-10.ps1
@@ -1,22 +1,22 @@
 <#
 ==========================================================================
-Description : Tous les services qui contient ce que l'utilistrauer a saisie coloré en vert
+Description : Tous les services qui contient ce que l'utilistrauer a saisie colorÃ© en vert
 
-Auteur : Noah Froment
+Auteur : FRTDev
 Date : 04/12/2024
 
 Version : 1
 ==========================================================================
 #>
 
-$Saisie = Read-Host "Veuillez saisir la liste des services qui va être suligné en vert"
+$Saisie = Read-Host "Veuillez saisir la liste des services qui va Ãªtre sulignÃ© en vert"
 
 # Importer le fichier CSV
 $ServicesCSV = Import-Csv -Path "C:\Users\noah.froment\Desktop\AdminSys_Powershell\contextecubsituation7\exercice2\resultats\services.csv"
 
 # Parcourir chaque ligne du CSV
 foreach ($Service in $ServicesCSV) {
-    # Vérifier si le nom du service contient "system"
+    # VÃ©rifier si le nom du service contient "system"
     if ($Service.Name -like ("*"+ $Saisie +"*")) {
         # Afficher le nom et le statut du service en vert
         Write-Host ($Service.Name, $Service.Status) -ForegroundColor Green
