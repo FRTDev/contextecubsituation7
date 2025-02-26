2ace8e4c64a07db31f72398d365593b701a95417
FRT Dev <52767575+FRTDev@users.noreply.github.com>
2025-02-26 09:06:26 +0100

    .

--- exercice1/exercice1-3.ps1
+++ exercice1/exercice1-3.ps1
@@ -2,7 +2,7 @@
 ==========================================================================
 Description : Lister le nom, l'id et la description des processus "svchost" (sous forme de table)
 
-Auteur : Noah Froment
+Auteur : FRTDev
 Date : 04/12/2024
 
 Version : 1
