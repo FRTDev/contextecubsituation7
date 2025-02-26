b339a1a658d4999269cc3d2516f0bf3bfbca6025
FRT Dev <52767575+FRTDev@users.noreply.github.com>
2025-02-26 09:06:23 +0100

    .

--- exercice1/exercice1-4.ps1
+++ exercice1/exercice1-4.ps1
@@ -2,7 +2,7 @@
 ==========================================================================
 Description : Lister le nom, l'id et la description du processus saisie par l'utilisateur
 
-Auteur : Noah Froment
+Auteur : FRTDev
 Date : 04/12/2024
 
 Version : 1
@@ -10,6 +10,6 @@ Version : 1
 #>
 
 # Demande de saisie
-$Process = Read-Host "Veuillez saisir le processus à filtrer"
+$Process = Read-Host "Veuillez saisir le processus Ã  filtrer"
 
 Get-Process | Where-Object {$_.Name -like $Process} | Select-Object Id, ProcessName, Description
