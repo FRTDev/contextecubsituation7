2dd539aa10e538adc91c4ce162d507477f0861f9
FRT Dev <52767575+FRTDev@users.noreply.github.com>
2025-02-26 09:06:19 +0100

    .

--- exercice1/exercice1-5.ps1
+++ exercice1/exercice1-5.ps1
@@ -2,7 +2,7 @@
 ==========================================================================
 Description : Lister le nom, l'id et la description du processus saisie par l'utilisateur avec une fonction
 
-Auteur : Noah Froment
+Auteur : FRTDev
 Date : 04/12/2024
 
 Version : 1
@@ -16,7 +16,7 @@ function InfoProcessus {
 }
 
 # Demande de saisie
-$Saisie = Read-Host "Veuillez saisir le processus à filtrer"
+$Saisie = Read-Host "Veuillez saisir le processus Ã  filtrer"
 
 # Appel de la fonction
 InfoProcessus -ProcessName $Saisie
